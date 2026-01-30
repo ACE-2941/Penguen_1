3464f1852f65fb5ceea85723920a19bc1e9a8bd8
ACE-2941 <uzmanyy@gmail.com>
2026-01-30 06:07:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -46,89 +46,47 @@ window.onkeydown = (e) => {
 };
 window.onkeyup = () => moveDir = 0;
 
-// --- MOBİL: SİLİNMEYİ ENGELLEYEN KONTROL ---
-function handleMobile(e) {
+// --- MOBİL: SİLİNMEYİ VE IŞINLANMAYI ENGELLEYEN MANTIK ---
+
+
+function updateMoveDir(e) {
     e.preventDefault();
-    if (!gameActive) {
-        if (gameOverTimer > 30) resetGame();
-        return;
-    }
     const rect = canvas.getBoundingClientRect();
     const touch = e.touches[0];
     const scaleX = canvas.width / rect.width;
     const canvasX = (touch.clientX - rect.left) * scaleX;
 
-    // Koordinatın geçerli bir sayı olduğundan emin ol (Silinmeyi önler)
-    if (!isNaN(canvasX)) {
-        moveDir = (canvasX < canvas.width / 2) ? -1 : 1;
+    if (!gameActive) {
+        if (gameOverTimer > 30) resetGame();
+        return;
     }
+    // Sadece ekranın yarısına göre yön ver
+    moveDir = (canvasX < canvas.width / 2) ? -1 : 1;
 }
 
 canvas.addEventListener("touchstart", (e) => {
-    handleMobile(e);
+    updateMoveDir(e);
     if (gameActive) jump();
 }, { passive: false });
 
 canvas.addEventListener("touchmove", (e) => {
-    handleMobile(e);
+    updateMoveDir(e); // Sürüklerken yönü güncelle
 }, { passive: false });
 
 canvas.addEventListener("touchend", (e) => {
     e.preventDefault();
-    moveDir = 0; 
+    moveDir = 0; // Parmağını çektiğin an penguen DURUR, silinip gidemez
 }, { passive: false });
-
-// --- OYUN FONKSİYONLARI ---
 
 function jump() {
     if (!penguin.isJumping && gameActive) {
         penguin.velocityY = -16;
         penguin.isJumping = true;
-        penguin.frameY = 2; // Zıplama animasyonu
+        penguin.frameY = 2;
         penguin.maxFrames = 2;
     }
 }
 
 function resetGame() {
     puan = 0; obstacles = []; gameActive = true;
-    gameOverTimer = 0; penguin.x = 130; penguin.y = 500;
-    penguin.velocityY = 0; timer = 0; moveDir = 0;
-}
-
-function update() {
-    if (!gameActive) {
-        gameOverTimer++;
-        return;
-    }
-
-    // Yürüme hızı yavaş ve stabil
-    penguin.x += moveDir * 4; 
-    
-    // Zıplama fiziği
-    penguin.y += penguin.velocityY;
-    penguin.velocityY += penguin.gravity;
-
-    if (penguin.y > 500) {
-        penguin.y = 500;
-        penguin.isJumping = false;
-        penguin.velocityY = 0;
-        penguin.frameY = 0; 
-        penguin.maxFrames = 5;
-    }
-
-    // Sınırlar: Penguenin ekrandan silinmesini/çıkmasını engeller
-    if (penguin.x < 0) penguin.x = 0;
-    if (penguin.x > canvas.width - penguin.w) penguin.x = canvas.width - penguin.w;
-
-    // Engel yönetimi
-    let oyunHizi = (puan < 100) ? 3 : 3 + (puan - 100) * 0.05;
-    if (++timer > 80) {
-        obstacles.push({ x: Math.random() * (canvas.width - 50), y: -100, w: 50, h: 80, hitW: 30, hitH: 60 });
-        timer = 0;
-    }
-
-    obstacles.forEach((o, i) => {
-        o.y += oyunHizi;
-        if (o.y > canvas.height) { obstacles.splice(i, 1); puan++; }
-        
-        let pCenterX = penguin.x +
+    gameOverTimer =
